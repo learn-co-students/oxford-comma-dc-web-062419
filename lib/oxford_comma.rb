8c92095a65f2914705df3494de93a62a9d20bab8
Ursula Miller <ursula.miller@gmail.com>
2019-06-15 04:50:00 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+  if array.count == 1
+    array.join
+  elsif array.count == 2
+    array.join(" and ")
+  else
+    tail_end = array.pop
+    array.join(", ") + ", and " + tail_end
+  end
 end
